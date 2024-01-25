2bcaaa8786e05400a8f142e340783766b34e72f7
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 15:03:53 +0200

    Add files via upload

---
+++ Arrays/PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int [] numbers = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    numbers[i] = number;
+}
+
+for (int i = n - 1; i >= 0; i--)
+{
+    Console.Write($"{numbers[i]} ");
+}
---
+++ Arrays/RoundingNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿string variables = Console.ReadLine();
+
+double[] variablesArray = variables.Split(' ').Select(double.Parse).ToArray();
+
+for (int i = 0; i < variablesArray.Length; i++)
+{
+    int roundedVariable = (int)Math.Round(variablesArray[i], MidpointRounding.AwayFromZero);
+
+    Console.WriteLine($"{variablesArray[i]} => {roundedVariable}");
+}
