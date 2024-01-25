bd2706b2f09d3b548407552eeda0bfcc7eab42fd
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 12:58:35 +0200

    Add files via upload

---
+++ Objects/Exercise/MoreExercise/03.SpeedRacing/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+2
+AudiA4 23 0.3
+BMW-M2 45 0.42
+Drive BMW-M2 56
+Drive AudiA4 5
+Drive AudiA4 13
+End
+   
+3
+AudiA4 18 0.34
+BMW-M2 33 0.41
+Ferrari-488Spider 50 0.47
+Drive Ferrari-488Spider 97
+Drive Ferrari-488Spider 35
+Drive AudiA4 85
+Drive AudiA4 50
+End  
+*/
+class Programm
+{
+    class Car 
+    {
+        public Car(string model, decimal fuelAmount, decimal fuelConsumption)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumption = fuelConsumption;
+        }
+        public string Model { get; set; }
+        public decimal FuelAmount { get; set; }
+        public decimal FuelConsumption { get; set;}
+        public decimal TravelDistance { get; set;}
+
+        public void DriveCar(decimal distance)
+        {
+            if (distance * FuelConsumption <= FuelAmount)
+            {
+                TravelDistance += distance;
+                FuelAmount -= distance * FuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient fuel for the drive");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        List<Car> cars = new List<Car>();
+        int numberOfCars = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfCars; i++)
+        {
+            string[] carArguments = Console.ReadLine().Split();
+
+            string model = carArguments[0];
+            decimal fuelAmount = decimal.Parse(carArguments[1]);
+            decimal fuelConsumption = decimal.Parse(carArguments[2]);
+
+            Car car = new Car(model, fuelAmount, fuelConsumption);
+            cars.Add(car);
+        }
+
+        string input = "";
+        while ((input = Console.ReadLine())!= "End")
+        {
+            string[] driveInput = input.Split();
+
+            cars.Find(x => x.Model == driveInput[1]).DriveCar(decimal.Parse(driveInput[2]));
+        }
+
+        foreach (Car car in cars)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelDistance}");
+        }
+    }
+}
---
+++ Objects/Exercise/MoreExercise/04.RawData/Program.cs
@@ -0,0 +1,81 @@
+﻿class Programm
+{
+    class Engine
+    {
+        public Engine(int engineSpeed, int enginePower)
+        {
+            EngineSpeed = engineSpeed;
+            EnginePower = enginePower;
+        }
+        public int EngineSpeed { get; set; }
+        public int EnginePower { get; set; }
+    }
+
+    class Cargo 
+    {   
+        public Cargo(int cargoWeight, string cargoType)
+        {
+            CargoWeight = cargoWeight;
+            CargoType = cargoType;
+        }
+        public int CargoWeight { get; set; }
+        public string CargoType { get; set; }
+    }
+
+    class Car 
+    {
+        public Car(string[] carInfo)
+        {
+            Model = carInfo[0];
+            Engine = new Engine(int.Parse(carInfo[1]), int.Parse(carInfo[2]));
+            Cargo = new Cargo(int.Parse(carInfo[3]), carInfo[4]);
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+
+
+
+    static void Main()
+    {
+        List<Car> cars = new List<Car>();
+        int lines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < lines; i++)
+        {
+            cars.Add(new Car(Console.ReadLine().Split()));
+        }
+
+        string type = Console.ReadLine();
+
+        if (type == "fragile")
+        {
+            foreach(Car car in cars.Where(c => c.Cargo.CargoType == type && c.Cargo.CargoWeight < 1000))
+            {
+                Console.WriteLine(car.Model);
+            }
+        }
+        else if (type == "flamable")
+        {
+            foreach (Car car in cars.Where(c => c.Cargo.CargoType == type && c.Engine.EnginePower > 250))
+            {
+                Console.WriteLine(car.Model);
+            }
+        }
+/*
+2
+ChevroletAstro 200 180 1000 fragile
+Citroen2CV 190 165 900 fragile
+fragile
+
+
+4
+ChevroletExpress 215 255 1200 flamable
+ChevroletAstro 210 230 1000 flamable
+DaciaDokker 230 275 1400 flamable
+Citroen2CV 190 165 1200 fragile
+flamable
+*/
+    }
+}
