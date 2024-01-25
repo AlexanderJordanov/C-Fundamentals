d344d7fd208e4ad2c6e6d21af576bb7180c3cec3
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 13:26:47 +0200

    Add files via upload

---
+++ TextProcessing/Exercise/01.ValidUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        string[] text = Console.ReadLine().Split(", ").ToArray();
+
+        foreach (string user in text)
+        {
+            if (user.Length < 3 || user.Length > 16)
+            {
+                continue;
+            }
+
+            bool isValid = user.All(c => char.IsLetterOrDigit(c) || c == '-' || c == '_');
+
+            if (isValid)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+
+}
---
+++ TextProcessing/Exercise/03.ExtractFile/Program.cs
@@ -0,0 +1,23 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        string filePath = Console.ReadLine();
+
+        string fileName = String.Empty;
+        string fileExtension = String.Empty;
+
+        int lastSeparatorIndex = filePath.LastIndexOf('\\');
+        int lastExtensionIndex = filePath.LastIndexOf(".");
+
+        if (lastSeparatorIndex != -1 && lastExtensionIndex != -1 && lastExtensionIndex > lastSeparatorIndex)
+        {
+            fileName = filePath.Substring(lastSeparatorIndex + 1, lastExtensionIndex - lastSeparatorIndex - 1);
+            fileExtension = filePath.Substring(lastExtensionIndex + 1);
+        }
+
+        Console.WriteLine($"File name: {fileName}");
+        Console.WriteLine($"File extension: {fileExtension}");
+    }
+
+}
