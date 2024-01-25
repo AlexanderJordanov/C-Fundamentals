3eef5f492e6565c116c32f28871734c01edf7318
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 12:56:41 +0200

    Add files via upload

---
+++ Objects/Exercise/MoreExercise/01.CompanyRoster/Program.cs
@@ -0,0 +1,69 @@
+﻿class Programm
+{/*
+
+4
+Peter 120.00 Development
+Tony 333.33 Marketing
+Jony 840.20 Development
+George 0.20 Nowhere
+
+ */
+    class Employee 
+    {
+        public Employee(string name, decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+        public string Name { get; set; }   
+        public decimal Salary { get; set; }
+    }
+
+    class Department 
+    {
+        public Department(string depName)
+        {
+            DepName = depName;
+        }
+        public string DepName { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public decimal TotalSalary { get; set; }
+
+        public void AddNewEmployee (string empName, decimal empSalary)
+        {
+            TotalSalary += empSalary;
+
+            Employees.Add(new Employee(empName, empSalary));
+        }
+
+    }
+
+
+    static void Main()
+    {
+        List<Department> departments = new List<Department>();
+
+        int lines = int.Parse(Console.ReadLine());
+
+       for (int i = 0; i < lines; i++)
+        {
+            string[] input = Console.ReadLine().Split();  
+            
+            if (!departments.Any(d => d.DepName == input[2]))
+            {
+                departments.Add(new Department(input[2]));
+            }
+
+            departments.Find(d => d.DepName == input[2]).AddNewEmployee(input[0], decimal.Parse(input[1]));
+        }
+
+        Department bestDepartment = departments.OrderByDescending(d => d.TotalSalary / d.Employees.Count()).First();
+
+        Console.WriteLine($"Highest Average Salary: {bestDepartment.DepName}");
+
+        foreach (var employee in bestDepartment.Employees.OrderByDescending(e => e.Salary))
+        {
+            Console.WriteLine($"{employee.Name} {employee.Salary:F2}");
+        }
+    }
+}
---
+++ Objects/Exercise/MoreExercise/02.OldestFamilyMember/Program.cs
@@ -0,0 +1,46 @@
+﻿class Programm 
+{ 
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Family
+    {
+        public List<Person> family
+        { get; set; } = new List<Person>();
+
+        public void AddMember(string[] personInfo)
+        {
+            Person newPerson = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+            family.Add(newPerson);
+        }
+
+        public Person GetOlderPerson()
+        {
+            return family.OrderByDescending(p => p.Age).First();
+        }
+    }
+    static void Main()
+    {
+        Family family = new Family();
+
+        int lines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < lines; i++)
+        {
+            family.AddMember(Console.ReadLine().Split());
+        }
+
+        Person oldestPerson = family.GetOlderPerson();
+
+        Console.WriteLine(oldestPerson.Name + " " +  oldestPerson.Age);
+    }
+}
