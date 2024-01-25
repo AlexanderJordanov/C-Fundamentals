d414931ae90b5e4ccfa95948fd137da2f48d51cf
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 12:35:59 +0200

    Add files via upload

---
+++ Arrays/Exercise/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] array = Console.ReadLine().Split();
+int longestSequenceCount = 0;
+string longestSequenceSymbol = "";
+
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    int count = 1;
+
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (array[i] == array[j])
+        {
+            count++;
+            if (longestSequenceCount <= count) 
+            { 
+            longestSequenceCount = count;
+            longestSequenceSymbol = array[i];
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+for (int i = 0; i < longestSequenceCount; i++)
+{
+    Console.Write($"{longestSequenceSymbol} ");
+}
---
+++ Arrays/Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int number = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        int sum = array[i] + array[j];
+
+        if (sum == number)
+        {
+            Console.WriteLine($"{array[i]} {array[j]}");
+        }
+    }
+}
