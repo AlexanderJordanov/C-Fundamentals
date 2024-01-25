9f207ec5fe564ca71446697e896892db1494e7b2
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 13:23:04 +0200

    Add files via upload

---
+++ AssociativeArrays/Exercise/04.SoftUniParking/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.InteropServices;
+
+class Programm
+{
+    static void Main()
+    {
+        Dictionary<string, User> users = new Dictionary<string, User>();
+        int numberOfCommands = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfCommands; i++)
+        {
+            string[] arguments = Console.ReadLine().Split();
+
+            string command = arguments[0];
+            string userName = arguments[1];
+
+            switch (command)
+            {
+                case "register":
+                    string licensePlate = arguments[2];
+                    User user = new User(userName, licensePlate);
+                    if (users.ContainsKey(userName))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlate}");
+                    }
+                    else
+                    {
+                        users.Add(userName, user);
+                        Console.WriteLine($"{userName} registered {licensePlate} successfully");
+                    }
+                    break;
+                case "unregister":
+                    if (users.ContainsKey(userName))
+                    {
+                        users.Remove(userName);
+                        Console.WriteLine($"{userName} unregistered successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: user {userName} not found");
+                    }
+                    break;
+            }
+        }
+
+        foreach (KeyValuePair<string, User> kvp in users)
+        {
+            Console.WriteLine(kvp.Value);
+        }
+    }
+
+    class User
+    {
+        public User(string username, string licensePlate)
+        {
+            Username = username;
+            LicensePlate = licensePlate;
+        }
+        public string Username { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Username} => {LicensePlate}";
+        }
+    }
+}
---
+++ AssociativeArrays/Exercise/05.Courses/Program.cs
@@ -0,0 +1,50 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        Dictionary<string, Course> courses = new Dictionary<string, Course>();
+        string input = default;
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] studentArguments = input.Split(" : ");
+            string courseName = studentArguments[0];
+            string studentName = studentArguments[1];
+
+            if (!courses.ContainsKey(courseName))
+            {
+                Course course = new Course(courseName);
+                courses.Add(courseName, course);
+            }
+
+            courses[courseName].StudentNames.Add(studentName);
+        }
+
+        foreach (KeyValuePair<string, Course> kvp in courses)
+        {
+            Console.WriteLine(kvp.Value);
+        }
+    }
+
+    class Course
+    {
+        public Course(string name)
+        {
+            Name = name;
+            StudentNames = new List<string>();
+        }
+        public string Name { get; set; }
+        public List<string> StudentNames { get; set; }
+
+        public override string ToString()
+        {
+            string result = $"{Name}: {StudentNames.Count}\n";
+
+            for (int i = 0; i < StudentNames.Count; i++)
+            {
+                result += $"-- {StudentNames[i]}\n";
+            }
+
+            return result.Trim();
+        }
+    }
+}
