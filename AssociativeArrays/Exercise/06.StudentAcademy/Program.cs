55cb214d58c33d93aa4854c438989a5992de70ff
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-25 13:24:13 +0200

    Add files via upload

---
+++ AssociativeArrays/Exercise/06.StudentAcademy/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+5
+John
+5.5
+John
+4.5
+Alice
+6
+Alice
+3
+George
+5
+   
+ */
+
+
+class Programm
+{
+    class Student
+    {
+        public Student(string name)
+        {
+            Name = name;
+            Grades = new List<decimal>();
+        }
+        public string Name { get; set; }
+        public List<decimal> Grades { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} -> {Grades.Average():f2}";
+        }
+    }
+
+    static void Main()
+    {
+        Dictionary<string, Student> students = new Dictionary<string, Student>();
+
+        int pairs = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < pairs; i++)
+        {
+            string name = Console.ReadLine();
+            decimal grade = decimal.Parse(Console.ReadLine());
+
+            if (!students.ContainsKey(name))
+            {
+                students.Add(name, new Student(name));
+            }
+
+            students[name].Grades.Add(grade);
+        }
+
+        IEnumerable<KeyValuePair<string, Student>> filteredStudents = students.Where(g => g.Value.Grades.Average() >= 4.50m);
+
+        foreach (var kvp in filteredStudents)
+        {
+            Console.WriteLine(kvp.Value);
+        }
+    }
+}
---
+++ AssociativeArrays/Exercise/07.CompanyUsers/Program.cs
@@ -0,0 +1,52 @@
+﻿class Programm
+{
+    class Company
+    {
+        public Company(string name)
+        {
+            Name = name;
+            Id = new List<string>();
+        }
+        public string Name { get; set; }
+        public List<string> Id { get; set; }
+
+        public override string ToString()
+        {
+            string result = Name + "\n";
+
+            foreach (var item in Id)
+            {
+                result += $"-- {item}\n";
+            }
+
+            return result.Trim();
+        }
+    }
+    static void Main()
+    {
+        Dictionary<string, Company> companies = new Dictionary<string, Company>();
+        string input = default;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] arguments = input.Split(" -> ");
+            string name = arguments[0];
+            string id = arguments[1];
+
+            if (!companies.ContainsKey(name))
+            {
+                companies.Add(name, new Company(name));
+            }
+
+            if (!companies[name].Id.Contains(id))
+            {
+                companies[name].Id.Add(id);
+            }
+        }
+
+        foreach (var kvp in companies)
+        {
+            Console.WriteLine(kvp.Value);
+        }
+    }
+
+}
