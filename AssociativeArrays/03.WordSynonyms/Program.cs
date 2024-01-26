14efd0a09aa631bae7bc817959b4a10d9a5951bc
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-26 11:46:57 +0200

    Add files via upload

---
+++ AssociativeArrays/03.WordSynonyms/Program.cs
@@ -0,0 +1,28 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Dictionary<string, string> wordSynonyms = new Dictionary<string, string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string word = Console.ReadLine();
+            string synonym = Console.ReadLine();
+
+            if (!wordSynonyms.ContainsKey(word))
+            {
+                wordSynonyms[word] = synonym;
+            }
+            else
+            {
+                wordSynonyms[word] += ", " + synonym;
+            }
+        }
+
+        foreach (KeyValuePair<string,string> kvp in wordSynonyms)
+        {
+            Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+        }
+    }
+}
