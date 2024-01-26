6a4af2030acb2441e46250fcbff719d4cdbcf3fe
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-26 11:44:51 +0200

    Add files via upload

---
+++ TextProcessing/04.TextFilter/Program.cs
@@ -0,0 +1,15 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        string[] bannedWords = Console.ReadLine().Split(", ").ToArray();
+        string text = Console.ReadLine();
+
+        foreach (var word in bannedWords)
+        {
+            text = text.Replace(word, new string('*', word.Length));
+        }
+        Console.WriteLine(text);
+    }
+
+}
---
+++ TextProcessing/05.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,39 @@
+﻿class Programm
+{
+    static void Main()
+    {
+        //string text = Console.ReadLine();
+
+        //Console.WriteLine(new string(text.Where(x => char.IsDigit(x)).ToArray()));
+        //Console.WriteLine(new string(text.Where(x => char.IsLetter(x)).ToArray()));
+        //Console.WriteLine(new string(text.Where(x => !char.IsLetter(x) && !char.IsDigit(x)).ToArray()));
+
+
+
+        char[] text = Console.ReadLine().ToCharArray();
+        string digits = string.Empty;
+        string letters = string.Empty;
+        string other = string.Empty;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsDigit(text[i]))
+            {
+                digits += text[i];
+            }
+            else if (char.IsLetter(text[i]))
+            {
+                letters += text[i];
+            }
+            else
+            {
+                other += text[i];
+            }
+        }
+
+        Console.WriteLine(digits);
+        Console.WriteLine(letters);
+        Console.WriteLine(other);
+    }
+
+}
