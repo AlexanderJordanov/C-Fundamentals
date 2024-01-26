a24aafa83112dfd74103bd72aceb3be0efdd08b1
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-26 11:54:09 +0200

    Add files via upload

---
+++ Lists/MoreExercise/04.MixedUpLists/Program.cs
@@ -0,0 +1,62 @@
+﻿List<int> firstList = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+List<int> secondList = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List<int> mixedList = new List<int>();
+
+for (int i = 0; i < firstList.Count; i++)
+{
+    for (int j = secondList.Count - 1; j >= 0; j--)
+    {
+        if (firstList.Count > 0 && secondList.Count > 0)
+        {
+            mixedList.Add(firstList[i]);
+            mixedList.Add(secondList[j]);
+            firstList.RemoveAt(i);
+            secondList.RemoveAt(j);
+        }
+    }
+}
+
+int[] range = new int[2];
+
+if (firstList.Count > 0)
+{
+    range[0] = firstList[0];
+    range[1] = firstList[1];
+}
+else if (secondList.Count > 0)
+{
+    range[0] = secondList[0];
+    range[1] = secondList[1];
+}
+
+List< int > orderedMixedList = new List< int >();
+if (range[0] > range[1])
+{
+    for (int i = 0; i < mixedList.Count; i++)
+    {
+        if (mixedList[i] < range[0] && mixedList[i] > range[1])
+        {
+            orderedMixedList.Add(mixedList[i]);
+        }
+    }
+}
+else if (range[1] > range[0])
+{
+    for (int i = 0; i < mixedList.Count; i++)
+    {
+        if (mixedList[i] < range[1] && mixedList[i] > range[0])
+        {
+            orderedMixedList.Add(mixedList[i]);
+        }
+    }
+}
+
+orderedMixedList.Sort();
+Console.WriteLine(string.Join(" ", orderedMixedList));
---
+++ Lists/MoreExercise/05.DrumSet/Program.cs
@@ -0,0 +1,47 @@
+﻿double budget = double.Parse(Console.ReadLine());
+
+List<int> drums = Console.ReadLine()
+    .Split(' ')
+    .Select(int.Parse)
+    .ToList();
+
+List<int> price = new List<int>();
+price.AddRange(drums);
+
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+{
+    int hit = int.Parse(command);
+
+    for (int i = 0; i < drums.Count; i++)
+    {
+        drums[i] -= hit;
+        if (drums[i] <= 0)
+        {
+            if (budget - (price[i] * 3) >= 0)
+            {
+                budget = budget - (price[i] * 3);
+                drums[i] = price[i];
+            }
+        }
+    }
+
+    for (int i = 0; i < drums.Count; i++)
+    {
+        if (drums[i] <= 0)
+        {
+            drums.Remove(drums[i]);
+        }
+    }
+}
+//foreach (var drum in drums)
+//{
+//    Console.Write(drum + " ");
+//}
+
+Console.WriteLine(string.Join(" ", drums));
+Console.WriteLine();
+Console.WriteLine($"Gabsy has {budget:f2}lv.");
+
+
