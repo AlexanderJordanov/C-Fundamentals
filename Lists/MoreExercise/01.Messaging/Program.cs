91503a9623d1e281d456b8b0a06db3f07ebff2f4
Alexander Jordanov <146542674+AlexanderJordanov@users.noreply.github.com>
2024-01-26 11:53:15 +0200

    Add files via upload

---
+++ Lists/MoreExercise/01.Messaging/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+9992 562 8933
+This is some message for you
+ */
+
+
+List<int> num = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List< char > text = new List< char >();
+text.AddRange(Console.ReadLine());
+
+for (int i = 0; i < num.Count; i++)
+{
+    int sum = 0;
+
+    while (num[i] != 0)
+    {
+        sum += num[i] % 10;
+        num[i] /= 10;
+    }
+
+    int times = sum / text.Count;
+    sum = sum - times * text.Count;
+
+    Console.Write(text[sum]);
+
+    text.RemoveAt(sum);
+}
---
+++ Lists/MoreExercise/02.CarRace/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 
+[29 13 9 0 13 0 21 0 14 82 12]
+ */
+
+
+List< int > race = Console.ReadLine()//double?
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+double firstRacer = 0;
+double secondRacer = 0;
+
+for (int i = 0; i < race.Count / 2; i++)
+{
+    firstRacer += race[i];
+
+    if (race[i] == 0)
+    {
+        firstRacer *= 0.8;
+    }
+}
+
+for (int i = race.Count - 1; i > race.Count / 2; i--)
+{
+    secondRacer += race[i];
+
+    if (race[i] == 0)
+    {
+        secondRacer *= 0.8;
+    }
+}
+
+if (firstRacer > secondRacer)
+{
+    Console.WriteLine($"The winner is right with total time: {Math.Round(secondRacer, 1)}");
+}
+else
+{
+    Console.WriteLine($"The winner is left with total time: {Math.Round(firstRacer, 1)}");
+}
